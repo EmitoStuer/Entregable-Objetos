aad07477de8f19b95aa49af511305e985137e1fd
EmitoStuer <stuer.87@gmail.com>
2024-09-26 16:55:01 -0300

    Modificacion de Registro Automotor y limpieza de Main

--- RegistroAutomotor.ts
+++ RegistroAutomotor.ts
@@ -13,7 +13,32 @@ export class RegistroAutomotor{
         this.listaMotos = [];
         this.listaCamiones = [];
     }
-    
+    opcionesVehiculos():void{
+        let opcionSeleccionada : number;
+        do {
+            console.log(`Opciones \n 1 - Agregar Vehiculo \n 2 - Ver Vehiculos \n 3 - Modificar un Vehiculo \n 4 - Eliminar Vehiculo \n 5 - Salir`);
+            opcionSeleccionada = rls.questionInt("Ingrese la Opcion que Desee: ");
+            while (opcionSeleccionada<1 || opcionSeleccionada>5){
+                opcionSeleccionada = rls.questionInt("Seleccione el numero que figura en la opcion: \n 1 - Agregar. \n 2 - Ver. \n 3 - Modificar. \n 4 - Eliminar. \n 5 - Salir ");
+            }
+            
+                switch (opcionSeleccionada){
+                    case 1 : this.agregarVehiculo();
+                    break;
+                    case 2 : this.verVehiculos();
+                    break;
+                    case 3 : this.modificarVehiculo();
+                    break;
+                    case 4 : this.eliminarVehiculo();
+                    break;
+                    case 5 : console.log("Saliendo...");
+                    break;
+                    default: console.log("Error de Datos");
+                }
+            
+        }while(opcionSeleccionada!==5);
+    }
+
     agregarVehiculo():void{
         let opcionSeleccionada : number ;
         do {
